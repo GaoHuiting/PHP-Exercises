511eb264824d22ec4df8756caab747917d321d19
GaoHuiting <46456042+GaoHuiting@users.noreply.github.com>
2019-01-09 11:37:19 -0500

    Add files via upload

---
+++ introduction-to-php.php
@@ -0,0 +1,112 @@
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+<?php
+    echo "<br>";
+    echo "Welcome to PHP!";
+    echo "<br>";
+?>
+
+
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+<?php
+    echo "<br>";
+    echo 'PHP Stands for "Hypertext Preprocessor"!';
+?>
+
+/* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+<?php
+    echo '<p>PHP Stands for "Hypertext Preprocessor"!</p>';
+?>
+
+/* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+
+   
+   
+   
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+<?php
+    echo "<br>";
+    echo "HTML - Stands for Hypertext Markup Language";
+    echo "<br>";
+    echo "CSS - Stands for Cascading Stylesheet";
+    echo "<br>";
+    echo "JS - Stands for JavaScript";
+    echo "<br>";
+    echo "PHP - Stands for Hypertext Preprocessor";
+    echo "<br>";
+    echo "SQL - Stands for Structural Query Language";
+    echo "<br>";
+?>
+
+/* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+
+
+<?php
+    echo "<ul>";
+        echo "<br>";
+        echo "<li>HTML - Stands for Hypertext Markup Language</li>";
+        echo "<br>";
+        echo "<li>CSS - Stands for Cascading Stylesheet</li>";
+        echo "<br>";
+        echo "<li>JS - Stands for JavaScript</li>";
+        echo "<br>";
+        echo "<li>PHP - Stands for Hypertext Preprocessor</li>";
+        echo "<br>";
+        echo "<li>SQL - Stands for Structural Query Language</li>";
+        echo "<br>";
+    echo "</ul>";
+?>
